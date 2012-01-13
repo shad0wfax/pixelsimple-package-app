f247aa76e3797388380bbc16f3f1c65131cfae5e
Akshay Sharma <cto@pixelsimple.com>
2012-01-13 09:56:42 +0530

    Init commit

---
+++ src/main/java/com/pixelsimple/packageapp/Packager.java
@@ -0,0 +1,21 @@
+/**
+ * © PixelSimple 2011-2012.
+ */
+package com.pixelsimple.packageapp;
+
+/**
+ *
+ * @author Akshay Sharma
+ * Jan 11, 2012
+ */
+public class Packager {
+
+	public static void main(String [] arg) {
+		Packager packager = new Packager();
+		packager.print();
+	}
+	
+	public void print() {
+		System.out.println("Hola, packaging");
+	}
+}
