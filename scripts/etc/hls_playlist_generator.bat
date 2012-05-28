31732e089a50efb2eb1f0114f113c97b725d688f
Akshay Sharma <cto@pixelsimple.com>
2012-05-28 10:41:27 +0530

    1) Added ninja option properties. 2) Playlist generator script changes for better segment time calculations

--- scripts/etc/hls_playlist_generator.bat
+++ scripts/etc/hls_playlist_generator.bat
@@ -1,7 +1,7 @@
 @echo off
-setlocal
+setlocal ENABLEDELAYEDEXPANSION
 
-REM Example usage: hls_playlist_generator.bat Z:\Downloads\Nova\ Z:\Downloads\Nova\hls_playlist.m3u8 ts 10 Z:\Downloads\Nova\hls_t.complete 10 /someurl
+REM Example usage: hls_playlist_generator.bat Z:\Downloads\Nova\ Z:\Downloads\Nova\hls_playlist.m3u8 ts 10 Z:\Downloads\Nova\pixelsimple_hls_transcode.complete 10  C:\dev\pixelsimple\ffprobe\32_bit\1.0\ffprobe.exe /someurl
 
 REM - mandatory - the hls_media_dir should have the trailing \ at the end. Simplifies the script.
 set hls_media_dir=%1
@@ -10,17 +10,22 @@ set hls_file_extension=%3
 set check_interval_in_sec=%4
 set hls_transcode_complete_file=%5
 set segment_time=%6
+set ffprobe_path=%7
 REM Stripping the end double quotes - The base uri can contain '=' param. Hence it needs to be passed with quotes (if passed) 
-set base_uri=%~7
+set base_uri=%~8
 
 REM Other variables
 set temp_playlist_file=%hls_media_dir%hls_temp.m3u8
 set log_file=%hls_media_dir%hls_log.txt
+REM a temp file to compute the ffprobe segment output. Will be removed later.
+set duration_file=%hls_media_dir%hls_segment_duration_file.pixelsimple
 set hls_file_start=#EXTM3U
-set hls_file_duration=#EXT-X-TARGETDURATION:%segment_time%
+REM target_duration is hard coded to segment_time for now. Can extend this to be a param if needed.
+set target_duration=%6
+set hls_file_duration=#EXT-X-TARGETDURATION:%target_duration%
 set hls_file_sequence=#EXT-X-MEDIA-SEQUENCE:0
-REM TODO: what is that 10 below? read the damn manual!
-set hls_segment_header=#EXTINF:10, no desc
+REM The description (after comma below) is left blank. Extend the script if needed.
+set hls_segment_header=#EXTINF:
 set hls_file_end=#EXT-X-ENDLIST
 set completed=false
 
@@ -34,13 +39,14 @@ echo.>%playlist_file%
 echo.>%log_file%
 
 echo Starting the transcoding at %date% %time% >> %log_file%
-echo %hls_media_dir% >> %log_file%
-echo %playlist_file% >> %log_file%
-echo %hls_file_extension% >> %log_file%
-echo %check_interval_in_sec% >> %log_file%
-echo %hls_transcode_complete_file% >> %log_file%
-echo %segment_time% >> %log_file%
-echo %base_uri% >> %log_file%
+echo hls_media_dir = %hls_media_dir% >> %log_file%
+echo playlist_file = %playlist_file% >> %log_file%
+echo hls_file_extension = %hls_file_extension% >> %log_file%
+echo check_interval_in_sec = %check_interval_in_sec% >> %log_file%
+echo hls_transcode_complete_file = %hls_transcode_complete_file% >> %log_file%
+echo segment_time = %segment_time% >> %log_file%
+echo ffprobe_path = %ffprobe_path% >> %log_file%
+echo base_uri = %base_uri% >> %log_file%
 
 goto until_hls_transcode_complete
 
@@ -63,21 +69,52 @@ goto until_hls_transcode_complete
 	echo %hls_file_sequence% >> %temp_playlist_file%
 		
 	for /f "usebackq tokens=*" %%f in (`dir /b %hls_media_dir% ^| findstr %hls_file_extension% 2^>nul:`) do (
-		echo %hls_segment_header% >> %temp_playlist_file%
+	
+		if %completed%==false (
+			REM If transcoding is in progress, use the supplied segment_time, else compute the actual time. 
+			REM Since if transcoding is in progress, we treat that as live streaming.
+			echo %hls_segment_header%%segment_time%, >> %temp_playlist_file%
+		) else (
+			REM Ensure media file is quoted in double quotes for ffprobe. Strip any quote off media dir first.
+			set mediaFile="%hls_media_dir:"=%%%~f"
+			echo Going to compute the actual video length size of !mediaFile! using ffprobe and rewrite #EXTINF tag >> %log_file%
+			
+			REM Execute ffprobe command and write it to duration file, then read it back to a variabnle.
+			REM Need to do this, since if ffprobe path contains double quotes, it fails to execute in for loop directly.
+			%ffprobe_path% -i !mediaFile! -print_format json -show_format 2>nul | findstr /I duration > %duration_file%
+			set /p ffprobeOutput=<%duration_file%
+			echo ffprobeOutput = !ffprobeOutput! >> %log_file%
+			
+			for /f "usebackq tokens=2 delims=:" %%g in ('!ffprobeOutput!') do (
+				REM convert it to an integer (basically drop off the decimal places, as per the spec)
+				REM TODO: Ideally it should rounded off to the nearest integer, but it is not done for now. Redirecting any conversion error to nul.
+				set /a dur=%%g 2>nul
+				
+				REM we just need 2 places after decimal (if present)
+				echo Duration of %%~f is %%g will use !dur!  >> %log_file%
+
+				REM no description added. Can extend in future to indlude segment descriptions if needed.
+				echo %hls_segment_header%!dur!, >> %temp_playlist_file%
+			)
+		)
+		
 		REM removing the quotes from media dir if present
-		echo %base_uri%%hls_media_dir:"=%%%~f >> %temp_playlist_file%
+		echo %base_uri%%%~f >> %temp_playlist_file%
 	)	
-	echo %hls_file_end% >> %temp_playlist_file%
+
 	copy /b %temp_playlist_file% %playlist_file% > nul
 	
 	if %completed%==false (
 		echo completed=%completed% at %date% %time%  >> %log_file%	
 		goto until_hls_transcode_complete
 	) else (
+		REM adding the end tag at the end of transcode. During transcoding, it will be considered a live-stream. This avoids bugs.
+		echo %hls_file_end% >> %playlist_file%
 		echo completed=%completed% Going to end! at %date% %time%  >> %log_file%	
 		goto end
 	)
 	
-	
 :end
-if exist %temp_playlist_file% del %temp_playlist_file% > nul 
+	if exist %temp_playlist_file% del %temp_playlist_file% > nul 
+	if exist %duration_file% del %duration_file% > nul 
+endlocal
--- scripts/etc/hls_playlist_generator.sh
+++ scripts/etc/hls_playlist_generator.sh
@@ -3,24 +3,14 @@
 # Params:
 # directory to monitor.
 # Playlist file (or should this create it?)
-# Time to check the directory repeatedly. (Default to time of each segment or sooner?)
-# Base URI (ex: nova/hls?fileName=)
-# Pattern of file getting written (follow ffmpeg pattern?)
-# Bit rate (validate why we need this)
+# Pattern of file getting written (ex: *.ts)
+# Time to check the directory repeatedly.
+# Transcode complete file to be checked, in order to assume transcoding is complete.
 # The time of each segment.
-# Number of files - (Optional? in case of live feed)
+# Ffprobe path to determine each segment time once transcode is complete.
+# Base URI (ex: nova/hls?fileName=)
 # 
 
-# Logic:
-# Set TIME=0;
-# START: Read the given directory in a loop every Ts (Where T = timxe to check directory repeatedly)
-#  - Look for a file of the pattern supplied that has been written 
-#  - 
-# 
-# 
-# 
-# 
-# END
 # 
 hls_media_dir=$1
 playlist_file=$2
@@ -28,14 +18,14 @@ hls_file_extension_pattern="\."$3"$"
 check_interval_in_sec=$4
 hls_transcode_complete_file=$5
 segment_time=$6
-base_uri=$7
+ffprobe_path=$7
+base_uri=$8
 temp_playlist_file="$hls_media_dir""temp.m3u8"
 log_file="$hls_media_dir""hls_log.txt"
 hls_file_start="#EXTM3U"
 hls_file_duration="#EXT-X-TARGETDURATION:"
 hls_file_sequence="#EXT-X-MEDIA-SEQUENCE:0"
-# TODO: what is that 10 below? read the damn manual!
-hls_segment_header="#EXTINF:10, no desc"
+hls_segment_header="#EXTINF:"
 hls_file_end="#EXT-X-ENDLIST"
 
 # TODO: Handle the issues of folder access here and exit the script with error (to avoid infinite loop)
@@ -47,17 +37,19 @@ touch "$playlist_file"
 touch "$temp_playlist_file"
 touch "$log_file"
 
+# target_duration is hard coded to segment_time for now. Can extend this to be a param if needed.
 hls_file_duration=$hls_file_duration$segment_time
 
 # Echo stuff to log
 echo "Starting hls transcoding at "  $(date) >> "$log_file"
-echo "$hls_media_dir" >> "$log_file"
-echo "$playlist_file" >> "$log_file"
-echo "$hls_file_extension_pattern" >> "$log_file"
-echo "$check_interval_in_sec" >> "$log_file"
-echo "$hls_transcode_complete_file" >> "$log_file"
-echo "$segment_time" >> "$log_file"
-echo "$base_uri" >> "$log_file"
+echo "hls_media_dir = " "$hls_media_dir" >> "$log_file"
+echo "playlist_file = " "$playlist_file" >> "$log_file"
+echo "hls_file_extension_pattern = " "$hls_file_extension_pattern" >> "$log_file"
+echo "check_interval_in_sec = " "$check_interval_in_sec" >> "$log_file"
+echo "hls_transcode_complete_file = " "$hls_transcode_complete_file" >> "$log_file"
+echo "segment_time = " "$segment_time" >> "$log_file"
+echo "ffrobe_path = " "$ffprobe_path" >> "$log_file"
+echo "base_uri = " "$base_uri" >> "$log_file"
 
 create_playlist(){
 
@@ -70,12 +62,41 @@ create_playlist(){
 
 #	for file in `ls "$hls_media_dir" | grep "$hls_file_extension_pattern" ` ; 
 	ls "$hls_media_dir" | grep "$hls_file_extension_pattern" | while read file;
-	do
-		echo "$hls_segment_header" >> "$temp_playlist_file"
-		echo "$base_uri$hls_media_dir""$file" >> "$temp_playlist_file"
+do
+
+        	if [ -e "$hls_transcode_complete_file" ]
+            then
+                # Logic to compute the actual file segment times.
+                ffprobe_cmd="\"$ffprobe_path\" -i \"$hls_media_dir$file\" -print_format json -show_format 2>/dev/null | grep -i duration | cut -d : -f 2 "
+                durationString=$(eval "$ffprobe_cmd")
+                # compute the duration by rounding off to a number. Split the string on decimal and compare the first digigt after decimal to round off.
+                durationInt=$(echo ${durationString} | cut -d . -f 1)
+                # Strip the quote
+                durationInt=${durationInt:1}
+                durationDecimal=$(echo ${durationString} | cut -d . -f 2)
+                # Get the first digit after decimal
+                durationDecimal=${durationDecimal:0:1}
+
+                if [[ $durationDecimal -ge 5 ]]
+                then
+                    durationInt=$(($durationInt + 1))
+                fi
+
+                echo "Duration of $file is" $durationString "but will use " $durationInt " and " $durationDecimal >> "$log_file"
+                # eval $"$ffprobe_path" -i "$hls_media_dir""$file" >> "$temp_playlist_file"
+        		echo "$hls_segment_header"$durationInt, >> "$temp_playlist_file"
+
+            else
+                # If transcoding is still in progress the segment time will be kept as the default segment_time
+                # Leaving segment description empty.
+        		echo "$hls_segment_header"$segment_time, >> "$temp_playlist_file"
+        
+            fi	
+
+
+		echo "$base_uri""$file" >> "$temp_playlist_file"
 	done
 	
-	echo "$hls_file_end" >> "$temp_playlist_file"
 	cat "$temp_playlist_file" > "$playlist_file"
 }
 
@@ -85,7 +106,9 @@ do
 	then
 		# create one last time and then exit
 		create_playlist
-		rm -f "$temp_playlist_file"
+        # Add the file end tag
+        echo "$hls_file_end" >> "$playlist_file"
+        rm -f "$temp_playlist_file"
 		echo "Going to exit at " $(date) >> "$log_file"
 		exit 0;
 	else
